97cb679657ebdc276219481a74553290a3da966e
MayVT <mvallstor@gmail.com>
2023-03-22 20:36:40 +0100

    Guardar

---
+++ pages/blog.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Blog</h1>
+    <p>Texto de blog</p>
+    <p><NuxtLink to="/">Volver</NuxtLink></p>
+  </div>
+</template>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Esto es el artículo numero {{ myId }}</h1>
+  </div>
+</template>
+<script setup>
+const route = useRoute()
+const myId = ref(route.params.id);
+</script>
